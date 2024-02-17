62ced7d5581131d6e3b36585b40a5d26c9b02e47
joannegton <wtg131998@gmail.com>
2024-02-17 16:23:52 -0300

    update

--- src/schemas/Projeto.js
+++ src/schemas/Projeto.js
@@ -11,10 +11,11 @@ const ProjetoSchema = new Schema({
     localization: { type: String },
     budget: { type: Number },
     userId: { type: Schema.Types.ObjectId, required: true, ref: "clienteUsers" },
-    userName: { type: String },
+    userNameCliente: { type: String },
     userAvaliacao: { type: Number },
     status: { type: String }, // Estado da ordem de projeto
     aceitouOrdemPor: { type: Schema.Types.ObjectId, ref: "pilotoUsers" },
+    userNamePiloto: { type: String },
     outrasInformacoes: { type: String }, // Outras informações relevantes
     created_at: { type: Date, default: Date.now() },
 });
@@ -26,9 +27,24 @@ ProjetoSchema.pre('save', async function (next) {
         if (!user) {
             throw new Error('User not found');
         }
-        this.userName = user.name;
+        this.userNameCliente = user.name;
         this.userAvaliacao = user.avaliacao;
         // Preencher informações adicionais, se necessário
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+ProjetoSchema.pre('save', async function (next) {
+    try {
+        const user = await this.model('pilotoUsers').findById(this.aceitouOrdemPor);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        this.userNamePiloto = user.name;
+        this.userAvaliacao = user.avaliacao;
+ 
         next();
     } catch (error) {
         next(error);
