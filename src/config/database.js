c249c751e4aa94086df1a39d5a0f4929dd26033e
Wellington Tavares Galbarini <72559274+Joannegton@users.noreply.github.com>
2024-02-29 08:15:47 -0300

    update

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function connectDb() {
   try {
     await mongoose.connect(process.env.TOKEN_MONGODB);
-    console.log("MongoDB Atlas connected!");
+    console.log("Conectado ao MongoDB");
   } catch (err) {
     console.log(err.message);
   }
